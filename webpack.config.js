5d5b29469bb5889e46f3ec09bdc408f892d11eb8
libraryaddict <github@lib.co.nz>
2021-11-21 17:36:25 +1300

    Everything is CamelCase, but final output is lowercase

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    mallhistory: "./src/MallHistory.ts",
+    MallHistory: "./src/MallHistory.ts",
   },
   mode: "production",
   module: {
