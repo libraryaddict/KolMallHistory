a2d0e2f1aa8735bdddd493560700ed522e7e7228
Build Script <>
2021-12-15 15:04:17 +0000

    Build Mafia files for commit 19e412eb96800bc516aa978ecad5cdb6894ca4c1

--- scripts/utils/mallhistory.js
+++ scripts/utils/mallhistory.js
@@ -39,28 +39,42 @@ __webpack_require__.r(__webpack_exports__);
 
 // EXPORTS
 __webpack_require__.d(__webpack_exports__, {
-  "MallHistory": () => (/* binding */ MallHistory)
+  "MallHistory": () => (/* binding */ MallHistory),
+  "MallRecord": () => (/* binding */ MallRecord),
+  "MallRecords": () => (/* binding */ MallRecords)
 });
 
 ;// CONCATENATED MODULE: external "kolmafia"
 const external_kolmafia_namespaceObject = require("kolmafia");
 ;// CONCATENATED MODULE: ./src/MallHistory.ts
-function _toConsumableArray(arr) {return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();}function _nonIterableSpread() {throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}function _iterableToArray(iter) {if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter);}function _arrayWithoutHoles(arr) {if (Array.isArray(arr)) return _arrayLikeToArray(arr);}function _createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function _unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return _arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);}function _arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function _defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function _createClass(Constructor, protoProps, staticProps) {if (protoProps) _defineProperties(Constructor.prototype, protoProps);if (staticProps) _defineProperties(Constructor, staticProps);return Constructor;}function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}var
+function _toConsumableArray(arr) {return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();}function _nonIterableSpread() {throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}function _iterableToArray(iter) {if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter);}function _arrayWithoutHoles(arr) {if (Array.isArray(arr)) return _arrayLikeToArray(arr);}function _createForOfIteratorHelper(o, allowArrayLike) {var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"];if (!it) {if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {if (it) o = it;var i = 0;var F = function F() {};return { s: F, n: function n() {if (i >= o.length) return { done: true };return { done: false, value: o[i++] };}, e: function e(_e) {throw _e;}, f: F };}throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}var normalCompletion = true,didErr = false,err;return { s: function s() {it = it.call(o);}, n: function n() {var step = it.next();normalCompletion = step.done;return step;}, e: function e(_e2) {didErr = true;err = _e2;}, f: function f() {try {if (!normalCompletion && it.return != null) it.return();} finally {if (didErr) throw err;}} };}function _unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return _arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);}function _arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function _defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}function _createClass(Constructor, protoProps, staticProps) {if (protoProps) _defineProperties(Constructor.prototype, protoProps);if (staticProps) _defineProperties(Constructor, staticProps);Object.defineProperty(Constructor, "prototype", { writable: false });return Constructor;}function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}
 
-MallRecord =
+var MallRecord = /*#__PURE__*/_createClass(
+/**
+ * Seconds since epoch
+ */
 
+/**
+ * Amount purchased in this record
+ */
 
+/**
+ * Amount each item was purchased for
+ */
 
 
 function MallRecord(date, amount, meat) {_classCallCheck(this, MallRecord);_defineProperty(this, "date", void 0);_defineProperty(this, "amount", void 0);_defineProperty(this, "meat", void 0);
   this.date = date;
   this.amount = amount;
   this.meat = meat;
-};var
+});
 
 
-MallRecords = /*#__PURE__*/function () {
+var MallRecords = /*#__PURE__*/function () {
 
+  /**
+   * Being seconds since epoch
+   */
 
 
   function MallRecords(jsonObject) {_classCallCheck(this, MallRecords);_defineProperty(this, "records", []);_defineProperty(this, "lastUpdated", 0);
@@ -75,14 +89,36 @@ MallRecords = /*#__PURE__*/function () {
       }} catch (err) {_iterator.e(err);} finally {_iterator.f();}
   }_createClass(MallRecords, [{ key: "getRecords", value:
 
-    function getRecords(days) {
-      var cutoff = this.lastUpdated - days * 60 * 60 * 24;
+    function getRecords(overPeriodOfDays) {
+      var cutoff = this.lastUpdated - overPeriodOfDays * 60 * 60 * 24;
 
       return this.records.filter((r) => r.date >= cutoff);
+    } }, { key: "getAmountSold", value:
+
+    function getAmountSold(overPeriodOfDays) {
+      return this.getRecords(overPeriodOfDays).
+      map((record) => record.amount).
+      reduce((a, b) => a + b, 0);
+    } }, { key: "getPriceSold", value:
+
+    function getPriceSold(overPeriodOfDays) {
+      var amount = this.getRecords(overPeriodOfDays).
+      map((record) => record.amount).
+      reduce((a, b) => a + b, 0);
+
+      if (amount == 0) {
+        return 0;
+      }
+
+      var totalPrice = this.getRecords(overPeriodOfDays).
+      map((record) => record.meat * record.amount).
+      reduce((a, b) => a + b, 0);
+
+      return Math.round(totalPrice / amount);
     } }]);return MallRecords;}();var
 
 
-MallAge;(function (MallAge) {MallAge[MallAge["HOURS_12"] = 5] = "HOURS_12";MallAge[MallAge["DAY"] = 1] = "DAY";MallAge[MallAge["WEEK"] = 2] = "WEEK";MallAge[MallAge["MONTH"] = 3] = "MONTH";MallAge[MallAge["ALL"] = 4] = "ALL";MallAge[MallAge["HOURS_48"] = 6] = "HOURS_48";MallAge[MallAge["WEEKS_2"] = 7] = "WEEKS_2";})(MallAge || (MallAge = {}));var
+MallAge;(function (MallAge) {MallAge[MallAge["HOURS_12"] = 5] = "HOURS_12";MallAge[MallAge["DAY"] = 1] = "DAY";MallAge[MallAge["HOURS_48"] = 6] = "HOURS_48";MallAge[MallAge["WEEK"] = 2] = "WEEK";MallAge[MallAge["WEEKS_2"] = 7] = "WEEKS_2";MallAge[MallAge["MONTH"] = 3] = "MONTH";MallAge[MallAge["ALL"] = 4] = "ALL";})(MallAge || (MallAge = {}));var
 
 
 
@@ -202,7 +238,7 @@ var MallHistory = /*#__PURE__*/function () {
       (0,external_kolmafia_namespaceObject.bufferToFile)(JSON.stringify(jsonObj), "mallhistory.txt");
     } }, { key: "ensureUpToDate", value:
 
-    function ensureUpToDate(item, daysToCheck) {
+    function ensureUpToDate(item, maxDaysOld) {
       var records = this.items.get(item);
 
       if (records == null) {
@@ -212,64 +248,55 @@ var MallHistory = /*#__PURE__*/function () {
 
       var currentDate = new Date().getTime() / 1000;
 
-      if (records.lastUpdated < currentDate - 7 * 24 * 60 * 60) {
-        new MallUpdater().updateMallRecords(item, records, MallAge.MONTH);
+      if (records.lastUpdated < currentDate - maxDaysOld * 24 * 60 * 60) {
+        new MallUpdater().updateMallRecords(
+        item,
+        records,
+        maxDaysOld > 30 ? MallAge.ALL : MallAge.MONTH);
+
 
         this.saveMallItems();
       }
+
+      return records;
     } }, { key: "getMallRecords", value:
 
     function getMallRecords(
-    item,
-    daysToCheck)
+    item)
 
-    {var maxDaysOldData = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 7;
-      this.ensureUpToDate(item, maxDaysOldData);
 
-      var records = this.items.get(item);
+    {var maxDaysOldData = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 7;var updateIfMissing = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+      if (!updateIfMissing) {
+        return this.items.get(item);
+      }
 
-      return records.getRecords(daysToCheck);
+      if (!this.items.has(item)) {
+        return this.ensureUpToDate(item, Math.max(1, maxDaysOldData));
+      } else if (maxDaysOldData > 0) {
+        return this.ensureUpToDate(item, maxDaysOldData);
+      }
+
+      return this.items.get(item);
     } }, { key: "getAmountSold", value:
 
     function getAmountSold(
     item,
-    daysToCheck)
+    overPeriodOfDays)
 
     {var maxDaysOldData = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 7;
-      this.ensureUpToDate(item, maxDaysOldData);
+      var records = this.ensureUpToDate(item, maxDaysOldData);
 
-      var records = this.items.get(item);
-
-      return records.
-      getRecords(daysToCheck).
-      map((record) => record.amount).
-      reduce((a, b) => a + b, 0);
+      return records.getAmountSold(overPeriodOfDays);
     } }, { key: "getPriceSold", value:
 
     function getPriceSold(
     item,
-    daysToCheck)
+    overPeriodOfDays)
 
     {var maxDaysOldData = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 7;
-      this.ensureUpToDate(item, maxDaysOldData);
+      var records = this.ensureUpToDate(item, maxDaysOldData);
 
-      var records = this.items.get(item);
-
-      var amount = records.
-      getRecords(daysToCheck).
-      map((record) => record.amount).
-      reduce((a, b) => a + b, 0);
-
-      if (amount == 0) {
-        return 0;
-      }
-
-      var totalPrice = records.
-      getRecords(daysToCheck).
-      map((record) => record.meat * record.amount).
-      reduce((a, b) => a + b, 0);
-
-      return Math.round(totalPrice / amount);
+      return records.getPriceSold(overPeriodOfDays);
     } }]);return MallHistory;}();
 var __webpack_export_target__ = exports;
 for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
